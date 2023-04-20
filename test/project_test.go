a2012f49299ec8dd26587999fbdb0e7d31a5a674
dimacgka <dimacgka@gmail.com>
2023-04-20 13:01:38 +0300

    [~] fixed tests functions

--- test/project_test.go
+++ test/project_test.go
@@ -1,57 +1,14 @@
 package test
 
-import (
-	"bank_api/internal/pkg/dependency"
-	"bank_api/pkg/thttp"
-	"github.com/stretchr/testify/assert"
-	"reflect"
-	"testing"
-)
-
-//=======================HTTP=======================//
-func TestRequest(t *testing.T) {
-	tdc := dependency.NewDependencyContainer().BuildDependencies().BuildContainer()
-
-	expected := map[string]interface{}{
-		"key": "value",
-	}
-	dest := map[string]interface{}{}
-	hc := tdc.ContainerInstance().Get("httpClient").(*thttp.ThttpClient)
-
-	result, statusCode, err := hc.Request("GET", "https://api.worldchange.cc/api/currencies/get_from", nil, nil, &dest, nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if statusCode != 200 {
-		t.Errorf("Request returned statusCode %d, expected 200", statusCode)
-	}
-	if !reflect.DeepEqual(result, expected) {
-		t.Errorf("Request returned %v, expected %v", result, expected)
-	}
-}
-
-func TestThttpClient_MakeQueryString(t *testing.T) {
-	hc := thttp.ThttpClient{}
-
-	t.Run("non-empty query params", func(t *testing.T) {
-		queryParams := map[string]interface{}{
-			"key1": "value1",
-			"key2": "value2",
-		}
-
-		expectedResult := "?key1=value1&key2=value2"
-
-		result := hc.MakeQueryString(queryParams)
-		assert.Equal(t, expectedResult, result)
-	})
-
-	t.Run("empty query params", func(t *testing.T) {
-		queryParams := map[string]interface{}{}
-
-		expectedResult := ""
-
-		result := hc.MakeQueryString(queryParams)
-
-		assert.Equal(t, expectedResult, result)
-	})
-}
+//import (
+//	"testing"
+//	testHttp "bank_api/pkg/thttp/test_http"
+//)
+//
+////=======================HTTP=======================//
+//type Test struct {
+//	test test
+//}
+//
+//func TestRun(t *testing.T) {
+//}
