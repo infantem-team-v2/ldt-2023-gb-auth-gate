6990796926041c2ee371d1568c3da4dbf95cc492
dimacgka <dimacgka@gmail.com>
2023-04-21 16:54:55 +0300

    [+] added functions consume and publish for RabitNQ

--- pkg/damqp/consumer.go
+++ pkg/damqp/consumer.go
@@ -1,1 +1,24 @@
 package damqp
+
+import (
+	amqp "github.com/rabbitmq/amqp091-go"
+	"log"
+)
+
+func Consume(ch *amqp.Channel, queueName string) {
+	msgs, err := ch.Consume(
+		queueName,
+		"",
+		true,
+		false,
+		false,
+		false,
+		nil)
+	log.Panicf("Error with register consumer: %s", err)
+
+	go func() {
+		for msg := range msgs {
+			log.Printf("Received a message: %s", msg.Body)
+		}
+	}()
+}
--- pkg/damqp/publisher.go
+++ pkg/damqp/publisher.go
@@ -1,1 +1,18 @@
 package damqp
+
+import (
+	"context"
+	amqp "github.com/rabbitmq/amqp091-go"
+	"log"
+	"time"
+)
+
+func Publish(ch *amqp.Channel, queueName string, text string) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	err := ch.PublishWithContext(ctx, "", queueName, false, false, amqp.Publishing{
+		ContentType: "text/plain",
+		Body:        []byte(text),
+	})
+	log.Panicf("Error with Publish: %s", err)
+}
